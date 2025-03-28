183a336f0bbb5f5ebe5e922d481a4fcbae973e7f
Parikshit Mali <parikshitmali17@gmail.com>
2025-03-28 14:16:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 // your code here
 
-let name=document.getElementById("name").value
-let year=documet.getElementById("year").value
+
 let btn=document.getElementById("button")
 let h=document.getElementById("url")
 
 
 btn.addEventListner("click",(e)=>{
-	h.innerText=`https://localhost:8080/${(name) ?  `/?name=${name} : ''`}` + `${(year) ? `/?$year=${year}` :''}`
-})
+let name=document.getElementById("name").value
+let year=documet.getElementById("year").value
+	let queryParameter=[];
+
+	
+	if(name) queryParameter.push(`name=${encodeURIComponent(name)}`)
+	if(year) queryParameter.push(`year=${encodeURIComponent(year)}`)
+
+	let queryString=queryParameter.length ? `?${queryParameter.join("&")} : ""`
+		h.innerText=`https://localhost:8080/${queryString}`
+}
