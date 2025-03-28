81c87ab89ac70457fbd523ad14a378e71cf06bfd
Parikshit Mali <parikshitmali17@gmail.com>
2025-03-28 13:46:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // your code here
+
+let name=document.getElementById("name").value
+let year=documet.getElementById("year").value
+let btn=document.getElementById("button")
+let h=document.getElementById("url")
+
+
+btn.addEventListner("click",(e)=>{
+	h.innerText=`https://localhost:8080/${(name) ?  `/?name=${name} : ''`}` + `${(year) ? `/?$year=${year}` :''}`
+})
